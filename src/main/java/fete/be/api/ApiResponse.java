6c126465455077dd91f3dd6011eb58d1a5e37f1b
KangGeonyoung <kky6335@gmail.com>
2024-05-19 00:59:21 +0900

    feat: 공통 Response #1 - ApiResponse 클래스 추가

---
+++ src/main/java/fete/be/api/ApiResponse.java
@@ -0,0 +1,25 @@
+package fete.be.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private Integer code;
+    private String message;
+    private T result;
+
+    public ApiResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public ApiResponse(Integer code, String message, T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+}
