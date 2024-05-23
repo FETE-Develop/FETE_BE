9076a2d16737b4fa78704418e39e722abb0989f4
KangGeonyoung <kky6335@gmail.com>
2024-05-23 17:52:26 +0900

    feat: Poster 도메인 수정 #1 - Event 타입 객체 변수를 사용

--- src/main/java/fete/be/domain/poster/Poster.java
+++ src/main/java/fete/be/domain/poster/Poster.java
@@ -1,36 +1,31 @@
 package fete.be.domain.poster;
 
-import fete.be.domain.participant.Participant;
+import fete.be.domain.event.Event;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 public class Poster {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "poster_id")
     private Long posterId;
 
-    private EventType eventType;  // 이벤트 종류 - FESTIVAL / PARTY
     private String title;  // 포스터 제목
+    private String posterImgUrl;  // 포스터 이미지
     private String institution;  // 기관명
     private String manager;  // 담당자
     private String managerContact;  // 담당자 연락처
-    private String startDate;  // 이벤트 시작일
-    private String endDate;  // 이벤트 종료일
-    private String address;  // 주소
     private String ticketName;  // 티켓 이름
     private String ticketPrice;  // 티켓 가격
-    private String description;  // 이벤트 관련 상세 설명
-    private String posterImgUrl;  // 포스터 이미지
-    private String mood;  // 파티 무드
 
-    @OneToMany(mappedBy = "poster", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Participant> participants = new ArrayList<>();  // 참여자 목록
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "event_id")
+    private Event event;  // 등록할 이벤트
 
+    private String createdAt;  // 생성일자
+    private String updatedAt;  // 수정일자
 }
