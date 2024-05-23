5283158bd8d6d929a1d04017fbde7147e7695d4b
KangGeonyoung <kky6335@gmail.com>
2024-05-23 17:47:54 +0900

    feat: Participant 도메인 추가 - 유저가 참여한 이벤트 및 결제 정보 포함

---
+++ src/main/java/fete/be/domain/event/Participant.java
@@ -0,0 +1,31 @@
+package fete.be.domain.event;
+
+import fete.be.domain.payment.Payment;
+import jakarta.persistence.*;
+import lombok.Getter;
+import fete.be.domain.member.Member;
+
+@Entity
+@Getter
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participant_id")
+    private Long participantId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;  // 참여한 회원
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;  // 유저가 참여한 이벤트
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "payment_id")
+    private Payment paymentState;  // 결제 상태
+
+    private String createdAt;  // 생성일자
+    private String updatedAt;  // 수정일자
+}
