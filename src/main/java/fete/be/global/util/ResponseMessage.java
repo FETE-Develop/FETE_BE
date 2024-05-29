fe0cd2fa94913e134ac43e0aae3128dde0bb4ad6
KangGeonyoung <kky6335@gmail.com>
2024-05-29 11:00:59 +0900

    feat: ResponseMessage enum - 응답 관련 상태코드 및 메시지 관리

---
+++ src/main/java/fete/be/global/util/ResponseMessage.java
@@ -0,0 +1,27 @@
+package fete.be.global.util;
+
+public enum ResponseMessage {
+
+    SUCCESS(200, "API 요청이 성공했습니다."),
+    INVALID_REQUEST(201, "불가능한 요청입니다."),
+    INTERNAL_ERROR(500, "내부 오류가 발생했습니다."),
+
+    // LOGIN
+    LOGIN_SUCCESS(1000, "로그인에 성공하였습니다."),
+    LOGIN_FAILURE(1001, "아이디 또는 비밀번호가 일치하지 않습니다.");
+
+    private int code;
+    private String message;
+
+    ResponseMessage(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
