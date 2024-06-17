3451f56bc3a3803c18a01fcf6630f8c3df386162
KangGeonyoung <kky6335@gmail.com>
2024-06-17 19:48:07 +0900

    feat: MemberRepository 관리 방식 변경 #1 - EntityManager 사용 -> JpaRepository 상속 방식으로 변경

--- src/main/java/fete/be/domain/member/persistence/MemberRepository.java
+++ src/main/java/fete/be/domain/member/persistence/MemberRepository.java
@@ -1,50 +1,13 @@
 package fete.be.domain.member.persistence;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    private final EntityManager em;
+    Optional<Member> findByEmail(String email);
 
-    /**
-     * 회원가입 API
-     * @param Member member
-     */
-    public void save(Member member) {
-        if (member.getMemberId() == null) {
-            em.persist(member);
-        } else {
-            em.merge(member);
-        }
-    }
-
-    public Optional<Member> findById(String email) {
-        return findAll().stream()
-                .filter(m -> m.getEmail().equals(email))
-                .findAny();
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class).getResultList();
-    }
-
-    public Boolean isExistEmail(String email) {
-        try {
-            Member member = em.createQuery("select m.email from Member m where m.email = :email", Member.class)
-                    .setParameter("email", email)
-                    .getSingleResult();
-            return true;
-        } catch (NoResultException e) {
-            return false;
-        }
-    }
+    boolean existsByEmail(String email);
 
 }
