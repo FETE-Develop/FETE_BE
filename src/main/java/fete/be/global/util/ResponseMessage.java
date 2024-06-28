8fab55d6ae779d6927a8b15e46b0c8a0445ddfb9
KangGeonyoung <kky6335@gmail.com>
2024-06-29 04:28:56 +0900

    feat: ResponseMessage 코드 추가 #4 #6 - EVENT API 관련 응답코드 및 메시지 추가

--- src/main/java/fete/be/global/util/ResponseMessage.java
+++ src/main/java/fete/be/global/util/ResponseMessage.java
@@ -17,7 +17,15 @@ public enum ResponseMessage {
     POSTER_SUCCESS(3000, "포스터 API 요청이 성공하였습니다."),
     POSTER_FAILURE(3001, "포스터 API 요청이 실패하였습니다."),
     POSTER_INVALID_POSTER(3002, "해당 포스터가 존재하지 않습니다."),
-    POSTER_INVALID_USER(3003, "해당 포스터의 작성자가 아닙니다."),;
+    POSTER_INVALID_USER(3003, "해당 포스터의 작성자가 아닙니다."),
+
+    // EVENT
+    EVENT_QR_SUCCESS(4000, "QR 코드 발급에 성공하였습니다."),
+    EVENT_QR_FAILURE(4001, "QR 코드 발급에 실패하였습니다."),
+    EVENT_INVALID_QR(4002, "일치하는 정보가 존재하지 않습니다."),
+    EVENT_VALID_QR(4003, "QR 코드가 인증되었습니다."),
+    EVENT_INVALID_FILE(4004, "잘못된 파일입니다."),
+    ;
 
     private int code;
     private String message;
