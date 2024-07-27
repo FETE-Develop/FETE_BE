007123a69a1f9305d8371668bb1a24ac3b1c3819
KangGeonyoung <kky6335@gmail.com>
2024-07-28 03:16:07 +0900

    feat: 외부 API를 호출하기 위한 설정 #4 - AppConfig: RestTemplate Bean 등록

--- src/main/java/fete/be/global/config/AppConfig.java
+++ src/main/java/fete/be/global/config/AppConfig.java
@@ -2,6 +2,7 @@ package fete.be.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
@@ -11,5 +12,10 @@ public class AppConfig {
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
