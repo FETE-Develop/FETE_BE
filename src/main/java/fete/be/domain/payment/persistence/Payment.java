453522ef988877db44fa68b25716ddea343041f8
KangGeonyoung <kky6335@gmail.com>
2024-07-28 03:20:03 +0900

    feat: Payment 엔티티 수정 #1 #10 - TossPayments API의 응답 값을 담기 위한 필드 추가 - updateTossFields: 토스 응답 값 업데이트 메서드

--- src/main/java/fete/be/domain/payment/persistence/Payment.java
+++ src/main/java/fete/be/domain/payment/persistence/Payment.java
@@ -2,6 +2,7 @@ package fete.be.domain.payment.persistence;
 
 import fete.be.domain.event.persistence.Event;
 import fete.be.domain.member.persistence.Member;
+import fete.be.domain.payment.application.dto.response.TossPaymentResponse;
 import fete.be.domain.ticket.persistence.Participant;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -27,12 +28,21 @@ public class Payment {
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "payment")
     private Participant participant;  // 유저의 이벤트 참여 정보
 
+    // 토스에서 제공하는 응답 값 (중요)-----------
+    private int totalAmount;  // 총 결제 금액
+    private String method;  // 결제 수단
+    private String paymentKey;  // 결제 키 값
+    private String orderId;  // 주문번호
+    //---------------------------------------
+
     @Column(name = "is_paid")
     private Boolean isPaid;  // 결제 상태 : 지불 = true, 미지불 = false
     @Column(name = "created_at")
     private LocalDateTime createdAt;  // 생성일자
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;  // 수정일자
+    @Column(name = "payment_at")
+    private LocalDateTime paymentAt;  // 결제일자
 
 
     // 생성 메서드
@@ -53,5 +63,18 @@ public class Payment {
     // 결제 완료로 전환
     public static void completePayment(Payment payment) {
         payment.isPaid = true;
+    }
+
+    // 토스 응답 값 업데이트 메서드
+    public static Payment updateTossFields(Payment payment, TossPaymentResponse tossPaymentResponse) {
+        payment.totalAmount = tossPaymentResponse.getTotalAmount();
+        payment.method = tossPaymentResponse.getMethod();
+        payment.paymentKey = tossPaymentResponse.getPaymentKey();
+        payment.orderId = tossPaymentResponse.getOrderId();
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        payment.paymentAt = currentTime;
+
+        return payment;
     }
 }
