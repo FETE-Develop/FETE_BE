8a1741d9dedf5e0a7bbd0d264ddb1b5f585eab7c
KangGeonyoung <kky6335@gmail.com>
2024-07-30 02:43:26 +0900

    feat: 카카오 로그인 환경설정 #4 #11 - build.gradle: OAuth 관련 의존성 추가 - AppConfig: ReactorResourceFactory, WebClient Bean으로 등록

--- src/main/java/fete/be/global/config/AppConfig.java
+++ src/main/java/fete/be/global/config/AppConfig.java
@@ -1,10 +1,21 @@
 package fete.be.global.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ReactorResourceFactory;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +28,25 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ReactorResourceFactory resourceFactory() {
+        ReactorResourceFactory factory = new ReactorResourceFactory();
+        factory.setUseGlobalResources(false);
+        return factory;
+    }
+
+    @Bean
+    public WebClient webClient() {
+        Function<HttpClient, HttpClient> mapper = client -> HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
+                .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(10))
+                        .addHandlerLast(new WriteTimeoutHandler(10)))
+                .responseTimeout(Duration.ofSeconds(1));
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(resourceFactory(), mapper);
+
+        return WebClient.builder().clientConnector(connector).build();
     }
 }
