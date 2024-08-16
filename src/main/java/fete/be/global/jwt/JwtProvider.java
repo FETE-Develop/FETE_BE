bcaa199f80c06bec3c4e06cd9b373f873d4f6451
KangGeonyoung <kky6335@gmail.com>
2024-08-16 22:41:34 +0900

    feat: AccessToken 유효 시간 임시 변경 #4 - 30분 -> 1일로 변경

--- src/main/java/fete/be/global/jwt/JwtProvider.java
+++ src/main/java/fete/be/global/jwt/JwtProvider.java
@@ -40,7 +40,8 @@ public class JwtProvider {
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim("auth", authorities)
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))  // 30분
+//                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))  // 30분
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))  // 1일
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
 
