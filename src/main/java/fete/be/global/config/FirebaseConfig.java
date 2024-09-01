6140c9c13ebd0247d294a78ebd70b0d71b149456
KangGeonyoung <kky6335@gmail.com>
2024-09-01 22:44:16 +0900

    feat: 푸시 알림 환경설정 #4 #15 build.gradle - Firebase 관련 의존성 추가
    FirebaseConfig
    - initFirebase: Firebase Admin SDK의 인증 정보 설정

---
+++ src/main/java/fete/be/global/config/FirebaseConfig.java
@@ -0,0 +1,26 @@
+package fete.be.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    FirebaseApp initFirebase() throws IOException {
+        InputStream serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
