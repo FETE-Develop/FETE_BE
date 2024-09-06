9bd11da1947990955f472ee1b0a75db8235e6898
KangGeonyoung <kky6335@gmail.com>
2024-09-06 14:58:22 +0900

    feat: 티켓 조회 응답에 필드 추가 #6 TicketDto - posterImage 필드 추가 - 커스텀 생성자 추가
    TicketService
    - getTickets, getTicketInfo: 커스텀 생성자 적용

--- src/main/java/fete/be/domain/ticket/application/dto/response/TicketDto.java
+++ src/main/java/fete/be/domain/ticket/application/dto/response/TicketDto.java
@@ -2,6 +2,7 @@ package fete.be.domain.ticket.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import fete.be.domain.event.persistence.EventType;
+import fete.be.domain.ticket.persistence.Participant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public class TicketDto {
     private Long participantId;
     private String eventName;
+    private String posterImage;  // 대표 이미지 1장
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime startDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
@@ -19,4 +21,15 @@ public class TicketDto {
     private String address;
     private String ticketType;
     private int ticketPrice;
+
+    public TicketDto(Participant participant) {
+        this.participantId = participant.getParticipantId();
+        this.eventName = participant.getEvent().getEventName();
+        this.posterImage = participant.getEvent().getPoster().getPosterImages().get(0).getImageUrl();
+        this.startDate = participant.getEvent().getStartDate();
+        this.endDate = participant.getEvent().getEndDate();
+        this.address = participant.getEvent().getAddress();
+        this.ticketType = participant.getPayment().getTicketType();
+        this.ticketPrice = participant.getPayment().getTicketPrice();
+    }
 }
