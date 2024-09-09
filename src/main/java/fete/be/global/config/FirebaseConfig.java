d231fd9c4106a65c59f321765b31c8c91dfc6cda
KangGeonyoung <kky6335@gmail.com>
2024-09-09 23:10:08 +0900

    fix: FirebaseConfig 임시 해결

--- src/main/java/fete/be/global/config/FirebaseConfig.java
+++ src/main/java/fete/be/global/config/FirebaseConfig.java
@@ -19,17 +19,18 @@ public class FirebaseConfig {
     FirebaseApp initFirebase() throws IOException {
         InputStream serviceAccount;
 
-        // 환경 변수를 통해 json 문자열 받기
-        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+//        // 환경 변수를 통해 json 문자열 받기
+//        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+//
+//        if (firebaseConfig != null) {
+//            // 환경 변수로 받은 json 문자열 읽기
+//            serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
+//        } else {
+//            // 로컬에서 json 파일 읽기
+//            serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
+//        }
 
-        if (firebaseConfig != null) {
-            // 환경 변수로 받은 json 문자열 읽기
-            serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
-        } else {
-            // 로컬에서 json 파일 읽기
-            serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
-        }
-
+        serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
