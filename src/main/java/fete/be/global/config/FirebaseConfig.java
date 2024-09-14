ed922e22d2f71820fa87de2aa2b6cf1fa7d2d684
KangGeonyoung <kky6335@gmail.com>
2024-09-14 12:02:37 +0900

    feat: FirebaseConfig 보안 설정 #4 Base64Util - Base64 인코딩, 디코딩 메서드

--- src/main/java/fete/be/global/config/FirebaseConfig.java
+++ src/main/java/fete/be/global/config/FirebaseConfig.java
@@ -3,19 +3,28 @@ package fete.be.global.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import fete.be.global.util.Base64Util;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.service-account}")
+    private String serviceAccountBase64;
+
     @Bean
     FirebaseApp initFirebase() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(Base64Util.decodeBytes(serviceAccountBase64));
+
+//        InputStream serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
