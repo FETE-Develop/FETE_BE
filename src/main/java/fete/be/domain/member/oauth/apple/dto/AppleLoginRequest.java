850b47e5059472fb59ae7a3268c4b80e7a2044e1
KangGeonyoung <kky6335@gmail.com>
2024-09-20 14:22:29 +0900

    feat: 애플 로그인 요청 DTO 생성자 추가 #8 AppleLoginRequest - 기본 생성자 추가

--- src/main/java/fete/be/domain/member/oauth/apple/dto/AppleLoginRequest.java
+++ src/main/java/fete/be/domain/member/oauth/apple/dto/AppleLoginRequest.java
@@ -2,11 +2,13 @@ package fete.be.domain.member.oauth.apple.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class AppleLoginRequest {
     private String idToken;  // 애플 응답 - idToken
 }
