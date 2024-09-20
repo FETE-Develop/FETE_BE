57f82ff43e61488fe1297f364b0f3976a02b37cb
KangGeonyoung <kky6335@gmail.com>
2024-09-21 02:28:30 +0900

    feat: 회원 프로필 조회 API 응답 변경 #9 GetMyProfileResponse - memberType 필드 추가 - 커스텀 생성자: 추가된 필드 반영

--- src/main/java/fete/be/domain/member/application/dto/response/GetMyProfileResponse.java
+++ src/main/java/fete/be/domain/member/application/dto/response/GetMyProfileResponse.java
@@ -2,12 +2,14 @@ package fete.be.domain.member.application.dto.response;
 
 import fete.be.domain.member.persistence.Gender;
 import fete.be.domain.member.persistence.Member;
+import fete.be.domain.member.persistence.MemberType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class GetMyProfileResponse {
+    private MemberType memberType;
     private String email;
     private String profileImage;
     private String userName;
@@ -17,6 +19,7 @@ public class GetMyProfileResponse {
     private String phoneNumber;
 
     public GetMyProfileResponse(Member member) {
+        this.memberType = member.getMemberType();
         this.email = member.getEmail();
         this.profileImage = member.getProfileImage().getImageUrl();
         this.userName = member.getUserName();
