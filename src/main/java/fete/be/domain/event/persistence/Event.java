0bf44a8385b2fb4cb1c4d898012147e0dfd0e1ae
KangGeonyoung <kky6335@gmail.com>
2024-09-29 00:17:09 +0900

    feat: 이벤트 상세설명 길이 제한 변경 #5 Event - description 필드의 길이 제한을 300 -> 1000으로 변경

--- src/main/java/fete/be/domain/event/persistence/Event.java
+++ src/main/java/fete/be/domain/event/persistence/Event.java
@@ -47,7 +47,7 @@ public class Event {
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Ticket> tickets = new ArrayList<>();  // 티켓 종류 및 가격 정보
 
-    @Column(name = "description", length = 300)
+    @Column(name = "description", length = 1000)
     private String description;  // 이벤트 관련 상세 설명
 
     @Column(name = "mood")
