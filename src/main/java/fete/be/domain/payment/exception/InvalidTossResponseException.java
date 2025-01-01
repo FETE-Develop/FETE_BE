f26d8513e22cf6b5b8fdabfdedc7c436fac59361
KangGeonyoung <kky6335@gmail.com>
2025-01-02 00:38:08 +0900

    feat: 토스페이먼츠 API 예외 처리 #10 EventController - buyTicket: 토스페이먼츠 API 장애에 대한 예외 처리
    TicketController
    - cancelTickets: 토스페이먼츠 API 장애에 대한 예외 처리
    
    TossService
    - executePayment, cancelPayment: 토스페이먼츠 API로부터 올바르지 않은 응답 값을 받는 경우에 대한 예외 처리 코드 추가
    
    ResponseMessage
    - TOSS 관련 코드 추가
    
    InvalidTossResponseException
    - 토스페이먼츠 API 장애에 대한 Exception

---
+++ src/main/java/fete/be/domain/payment/exception/InvalidTossResponseException.java
@@ -0,0 +1,7 @@
+package fete.be.domain.payment.exception;
+
+public class InvalidTossResponseException extends RuntimeException {
+    public InvalidTossResponseException(String message) {
+        super(message);
+    }
+}
