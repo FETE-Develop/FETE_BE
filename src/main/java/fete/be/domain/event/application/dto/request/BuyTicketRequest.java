3b09ec9a2cadb329ffd2cf57e99e10aa2ae0613e
KangGeonyoung <kky6335@gmail.com>
2025-02-22 00:18:26 +0900

    feat: 결제 관련 API의 DTO 생성자 추가 #10 BuyTicketDto, BuyTicketRequest - 생성자 추가

--- src/main/java/fete/be/domain/event/application/dto/request/BuyTicketRequest.java
+++ src/main/java/fete/be/domain/event/application/dto/request/BuyTicketRequest.java
@@ -1,6 +1,7 @@
 package fete.be.domain.event.application.dto.request;
 
 import fete.be.domain.payment.application.dto.request.TossPaymentRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class BuyTicketRequest {
     private List<BuyTicketDto> tickets;
     private TossPaymentRequest tossPaymentRequest;
